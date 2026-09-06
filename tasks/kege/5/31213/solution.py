995b59a08efcbf41dd5972020cf03611e0dd3ab2
vkost <daniil.verevkin2010@mail.ru>
2026-09-06 20:19:00 +0600

    я решил задачу

--- tasks/kege/5/31213/solution.py
+++ tasks/kege/5/31213/solution.py
@@ -1,9 +1,16 @@
-def solve(*args, **kwargs):
-    """
-    Основная логика решения.
-    """
-    pass
+def f(a):
+    b=""
+    while a>0:
+        b=str(a%2)+b
+        a=a//2
+    return b
 
-
-if __name__ == "__main__":
-    print(solve())
+t=8e10
+for a in range(17,1000):
+    b=f(a)
+    if a%2==0:
+        b='10'+b
+    else:
+        b='1'+b+'01'
+    t=min(t,int(b,2))
+print(t)

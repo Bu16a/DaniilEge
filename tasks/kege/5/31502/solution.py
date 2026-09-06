2747642ab480d809eee6ee72c5a614cbb911a420
vkost <daniil.verevkin2010@mail.ru>
2026-09-06 20:43:42 +0600

    1

--- tasks/kege/5/31502/solution.py
+++ tasks/kege/5/31502/solution.py
@@ -1,9 +1,18 @@
-def solve(*args, **kwargs):
-    """
-    Основная логика решения.
-    """
-    pass
+def f(a):
+    b=""
+    while a>0:
+        b=str(a%2)+b
+        a=a//2
+    return b
 
-
-if __name__ == "__main__":
-    print(solve())
+t=8e10
+for a in range(1,1000):
+    b=f(a)
+    if a%2==0:
+        b='11'+b+'11'
+    else:
+        b='1'+b+'00'
+    t=int(b,2)
+    if t>95:
+        print(t)
+        break

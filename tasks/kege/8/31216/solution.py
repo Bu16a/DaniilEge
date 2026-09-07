41c6987b05b549cf31b682604af0cb1a17195259
vkost <daniil.verevkin2010@mail.ru>
2026-09-07 17:41:28 +0600

    Задача решина верно

--- tasks/kege/8/31216/solution.py
+++ tasks/kege/8/31216/solution.py
@@ -1,9 +1,18 @@
-def solve(*args, **kwargs):
-    """
-    Основная логика решения.
-    """
-    pass
+from itertools import product
+count = 0
+nom=0
+a = product('ВЕКОТЦ', repeat=6)
+for i in a:
+    s = ''.join(i)
+    nom +=1
+    if s.count('О')!= 3:
+        continue
+    if s.count('Т')!= 2:
+        continue
+    if s.count('Ц')!= 1:
+        continue
+    if nom%2==0:
+        continue
+    count += 1
+print(count)
 
-
-if __name__ == "__main__":
-    print(solve())

b03428f4feeff90518017b678ab37bbbcabbb932
vkost <daniil.verevkin2010@mail.ru>
2026-09-16 19:58:15 +0600

    ujnj

--- tasks/kege/2/27757/solution.py
+++ tasks/kege/2/27757/solution.py
@@ -1,7 +1,7 @@
-for x in range(0, 2):
-    for y in range(0, 2):
-        for w in range(0, 2):
-            for z in range(0, 2):
+for x in range(2):
+    for y in range(2):
+        for w in range(2):
+            for z in range(2):
                 if (not (x) and y and z and not (w)) or (not (x) and y and not (z) and not (w)) or (
                         x and y and z and not (w)):
                     print(x, w, z, y)

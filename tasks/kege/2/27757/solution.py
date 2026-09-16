0e8d495f486d89ab8cf8d9a5bdff6088e39d9e69
vkost <daniil.verevkin2010@mail.ru>
2026-09-16 19:31:50 +0600

    сделал

--- tasks/kege/2/27757/solution.py
+++ tasks/kege/2/27757/solution.py
@@ -1,9 +1,7 @@
-def solve(*args, **kwargs):
-    """
-    Основная логика решения.
-    """
-    pass
-
-
-if __name__ == "__main__":
-    print(solve())
+for x in range(0, 2):
+    for y in range(0, 2):
+        for w in range(0, 2):
+            for z in range(0, 2):
+                if (not (x) and y and z and not (w)) or (not (x) and y and not (z) and not (w)) or (
+                        x and y and z and not (w)):
+                    print(x, w, z, y)

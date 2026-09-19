65517322e0fbc94a7795c1557157942f967e319a
vkost <daniil.verevkin2010@mail.ru>
2026-09-19 16:45:32 +0600

    Как я помню надо было делать 8 номера.

--- tasks/kege/8/31216/solution.py
+++ tasks/kege/8/31216/solution.py
@@ -1,8 +1,23 @@
-def solve(*args, **kwargs):
-    """
-    Основная логика решения.
-    """
+from itertools import product
 
+count = 0
+t=0
+a = product('ВЕКОТЦ', repeat=6)
+for i in a:
+    t+=1
+    if t%2==0:
+        continue
+    s = ''.join(i)
 
-if __name__ == "__main__":
-    print(solve())
+    if s.count('Т') != 2:
+        continue
+
+    if s.count('Ц') != 1:
+        continue
+
+    if s.count('О') != 3:
+        continue
+
+    count += 1
+
+print(count)
